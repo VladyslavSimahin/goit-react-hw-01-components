b29d965437431064510e948cab26ae5cb9a286ef
VladyslavSimahin <vladyslav.simahin@gmail.com>
2023-08-10 19:20:55 -0600

    new

---
+++ src/components/ProfilSocial/profil.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { Container } from './Profil.styled';
+import { ProfilList } from './profilList';
+import { ProfilSocial } from './profilSocial';
+
+export const Profil = ({
+  username,
+  location,
+  tag,
+  avatar,
+  stats: { likes, followers, views },
+}) => {
+  return (
+    <Container>
+      <ProfilSocial
+        username={username}
+        location={location}
+        tag={tag}
+        avatar={avatar}
+      />
+      <ProfilList likes={likes} views={views} followers={followers} />
+    </Container>
+  );
+};
+Profil.propTypes = {
+  username: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  time: PropTypes.exact({
+    likes: PropTypes.number.isRequired,
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+  }),
+};
